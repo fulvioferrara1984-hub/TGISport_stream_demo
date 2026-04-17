147cb3152cdf0a7a6f536ab5d6375f6839fafa57
fulvioferrara1984-hub <fulvio.ferrara1984@gmail.com>
2026-04-17 18:00:40 +0200

    Update users.js

--- users.js
+++ users.js
@@ -5,18 +5,18 @@ const USERS = {
     streamUrl: "https://stream.tgisportvideoplayer.video/cam1/index.m3u8"
   },
   user2: {
-    name: "Emma Collins1",
+    name: "Emma Collins",
     description: "Female, 20–30 years old, interested in fashion and digital media, Sales area.",
-    streamUrl: "http://178.104.195.106:8888/cam2/index.m3u8"
+    streamUrl: "https://stream.tgisportvideoplayer.video/cam2/index.m3u8"
   },
   user3: {
     name: "Daniel Brooks",
     description: "Male, 30–40 years old, interested in innovation and business development, Operations area.",
-    streamUrl: "http://178.104.195.106:8888/cam3/index.m3u8"
+    streamUrl: "https://stream.tgisportvideoplayer.video/cam3/index.m3u8"
   },
   user4: {
     name: "Sophia Turner",
     description: "Female, 20–30 years old, interested in communication and customer experience, HR area.",
-    streamUrl: "http://178.104.195.106:8888/cam4/index.m3u8"
+    streamUrl: "https://stream.tgisportvideoplayer.video/cam3/index.m3u8"
   }
 };
