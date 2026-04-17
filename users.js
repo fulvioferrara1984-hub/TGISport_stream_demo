4b088d4197ab91b1c0157181f19a2a891c84faa4
fulvioferrara1984-hub <fulvio.ferrara1984@gmail.com>
2026-04-17 13:55:10 +0200

    Update users.js

--- users.js
+++ users.js
@@ -2,21 +2,21 @@ const USERS = {
   user1: {
     name: "Michael Carter",
     description: "Male, 20–30 years old, interested in sports and technology, Marketing area.",
-    streamUrl: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+    streamUrl: "http://178.104.195.106:8888/cam1/index.m3u8"
   },
   user2: {
     name: "Emma Collins",
     description: "Female, 20–30 years old, interested in fashion and digital media, Sales area.",
-    streamUrl: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+    streamUrl: "http://178.104.195.106:8888/cam2/index.m3u8"
   },
   user3: {
     name: "Daniel Brooks",
     description: "Male, 30–40 years old, interested in innovation and business development, Operations area.",
-    streamUrl: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+    streamUrl: "http://178.104.195.106:8888/cam3/index.m3u8"
   },
   user4: {
     name: "Sophia Turner",
     description: "Female, 20–30 years old, interested in communication and customer experience, HR area.",
-    streamUrl: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+    streamUrl: "http://178.104.195.106:8888/cam4/index.m3u8"
   }
 };
